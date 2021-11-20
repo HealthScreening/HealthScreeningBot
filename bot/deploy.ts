d43d7454d9bc511ae0dc91746cec3be3a82ec412
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2021-11-20 14:10:15 -0500

    Refactor deploy script

---
+++ bot/deploy.ts
@@ -0,0 +1,32 @@
+/**
+ * Documentation can be found at https://pokestarfan.ga/docs/developer-documentation/api-reference/bot/deploy-ts/.
+ */
+import {REST} from '@discordjs/rest';
+import {Routes} from 'discord-api-types/v9';
+
+import { readdirSync } from 'fs';
+
+const {discord} = require("../config.json");
+
+function getCommands(): Object[] {
+    const commands = [];
+    const commandFiles = readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+        const command = require(`./commands/${file}`);
+        commands.push(command.data.toJSON());
+    }
+    return commands;
+}
+
+
+const rest = new REST({version: '9'}).setToken(discord.token);
+
+function registerCommands(commands: Object[]) {
+    rest.put(
+        Routes.applicationCommands(discord.clientId),
+        {body: commands},
+    ).then(() => console.log('Successfully registered application commands.')).catch(console.error)
+}
+
+registerCommands(getCommands());
