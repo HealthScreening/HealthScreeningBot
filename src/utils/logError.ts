b01842a39ce9a6a316281235fd4d1b6afeb22701
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2021-12-12 13:39:12 -0500

    Create function to autolog

---
+++ src/utils/logError.ts
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C) 2021 PythonCoderAS
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+import { ErrorLog } from "../orm/errorLog";
+
+export default async function logError(error: Error, metadata?: object): Promise<ErrorLog>{
+  const trueMetadata: object | null = metadata || null;
+  const errorName: string = error.name;
+  const errorMessage: string | null = error.message.length > 0 ? error.message : null;
+  const errorStack: string | null = error.stack || null;
+  return await ErrorLog.create({
+    errorName,
+    errorDescription: errorMessage,
+    errorStack,
+    metadata: trueMetadata
+  });
+}
