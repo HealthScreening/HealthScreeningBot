5bdf843c47fb7a876b6312cb58f98df0c44116b2
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2021-12-18 10:43:23 -0500

    Prettier

--- src/utils/logError/ignoreError.ts
+++ src/utils/logError/ignoreError.ts
@@ -1,14 +1,11 @@
 import { isEqual } from "lodash";
 import * as errorLogIgnore from "../../../errorLogIgnore.json";
 
-export default function ignoreError(
-  error: Error,
-  type: string
-): boolean {
+export default function ignoreError(error: Error, type: string): boolean {
   const comparsionObject = {
     type,
     name: error.name,
     message: error.message,
-  }
-  return errorLogIgnore.some(item => isEqual(comparsionObject, item));
+  };
+  return errorLogIgnore.some((item) => isEqual(comparsionObject, item));
 }
--- src/utils/logError/index.ts
+++ src/utils/logError/index.ts
@@ -22,7 +22,7 @@ export default async function logError(
   type: string,
   metadata?: object
 ): Promise<ErrorLog | null> {
-  if (ignoreError(error, type)){
+  if (ignoreError(error, type)) {
     return null;
   }
   const trueMetadata: object | null = metadata || null;
