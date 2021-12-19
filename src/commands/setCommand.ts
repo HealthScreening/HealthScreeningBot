899c6b5721f284651d3b29bb26c15571705efc95
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2021-12-19 13:18:14 -0500

    Add capability to set command for days

--- src/commands/setCommand.ts
+++ src/commands/setCommand.ts
@@ -15,9 +15,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 import { SlashCommandBuilder } from "@discordjs/builders";
-import screeningTypes, {
-  screeningTypeType,
-} from "@healthscreening/screening-types";
+import screeningTypes, { screeningTypeType } from "@healthscreening/screening-types";
 import { User } from "discord.js";
 import { devices } from "puppeteer";
 import createOrUpdate from "../utils/createOrUpdate";
@@ -25,6 +23,7 @@ import { Devices, DevicesAttributes } from "../orm/devices";
 import { AutoUser } from "../orm/autoUser";
 import { ItemType } from "../utils/multiMessage";
 import { HSBCommandInteraction } from "../discordjs-overrides";
+import { AutoDays } from "../orm/autoDays";
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -78,7 +77,42 @@ module.exports = {
         .setName("paused")
         .setDescription("Whether auto screenings should be paused.")
         .setRequired(false)
-    ),
+    ).addBooleanOption((option) => option
+      .setName("sunday")
+      .setDescription("Whether to run the screening on Sunday.")
+      .setRequired(false)
+    ).addBooleanOption((option) => option
+      .setName("monday")
+      .setDescription("Whether to run the screening on Monday.")
+      .setRequired(false)
+    ).addBooleanOption((option) => option
+      .setName("tuesday")
+      .setDescription("Whether to run the screening on Tuesday.")
+      .setRequired(false)
+    ).addBooleanOption((option) => option
+      .setName("wednesday")
+      .setDescription("Whether to run the screening on Wednesday.")
+      .setRequired(false)
+    )
+    .addBooleanOption((option) =>
+      option
+        .setName("thursday")
+        .setDescription("Whether to run the screening on Thursday.")
+        .setRequired(false)
+    )
+    .addBooleanOption((option) =>
+      option
+        .setName("friday")
+        .setDescription("Whether to run the screening on Friday.")
+        .setRequired(false)
+    )
+    .addBooleanOption((option) =>
+      option
+        .setName("saturday")
+        .setDescription("Whether to run the screening on Saturday.")
+        .setRequired(false)
+    )
+  ,
   async execute(interaction: HSBCommandInteraction) {
     const validDevices = Object.keys(devices);
     const deviceName = interaction.options.getString("device");
@@ -87,37 +121,49 @@ module.exports = {
     const type = interaction.options.getString("type");
     const emailOnly = interaction.options.getBoolean("email_only");
     const paused = interaction.options.getBoolean("paused");
+    const sunday = interaction.options.getBoolean("sunday");
+    const monday = interaction.options.getBoolean("monday");
+    const tuesday = interaction.options.getBoolean("tuesday");
+    const wednesday = interaction.options.getBoolean("wednesday");
+    const thursday = interaction.options.getBoolean("thursday");
+    const friday = interaction.options.getBoolean("friday");
+    const saturday = interaction.options.getBoolean("saturday");
     if (deviceName && !validDevices.includes(deviceName)) {
       return await interaction.reply({
         content:
           "Invalid device name! Please enter a valid device name. See the list of valid device names by using the `/device_list` command.",
-        ephemeral: true,
+        ephemeral: true
       });
     }
     const userOptions = await AutoUser.findOne({
       where: {
-        userId: interaction.user.id,
-      },
+        userId: interaction.user.id
+      }
     });
-    if (!userOptions && (hour || minute || type || emailOnly || paused)) {
+    const dayOptions = await AutoDays.findOne({
+      where: {
+        userId: interaction.user.id
+      }
+    });
+    if ((!userOptions && (hour || minute || type || emailOnly || paused)) || (!dayOptions && (sunday || monday || tuesday || wednesday || thursday || friday || saturday))) {
       return await interaction.reply({
         content:
           "You must first auto information using the `/set_auto` command.",
-        ephemeral: true,
+        ephemeral: true
       });
     }
     if (hour && (hour < 0 || hour > 23)) {
       return await interaction.reply({
         content:
           "Invalid hour! Please enter a valid hour in the range [0, 23].",
-        ephemeral: true,
+        ephemeral: true
       });
     }
     if (minute && (minute < 0 || minute > 59)) {
       return await interaction.reply({
         content:
           "Invalid minute! Please enter a valid minute in the range [0, 59].",
-        ephemeral: true,
+        ephemeral: true
       });
     }
     if (deviceName) {
@@ -125,7 +171,7 @@ module.exports = {
         Devices,
         {
           userId: interaction.user.id,
-          device: deviceName,
+          device: deviceName
         },
         { userId: interaction.user.id }
       );
@@ -148,13 +194,37 @@ module.exports = {
       }
       await userOptions.save();
     }
+    if (dayOptions){
+      if (sunday !== null) {
+        dayOptions.onSunday = sunday;
+      }
+      if (monday !== null) {
+        dayOptions.onMonday = monday;
+      }
+      if (tuesday !== null) {
+        dayOptions.onTuesday = tuesday;
+      }
+      if (wednesday !== null) {
+        dayOptions.onWednesday = wednesday;
+      }
+      if (thursday !== null) {
+        dayOptions.onThursday = thursday;
+      }
+      if (friday !== null) {
+        dayOptions.onFriday = friday;
+      }
+      if (saturday !== null) {
+        dayOptions.onSaturday = saturday;
+      }
+      await dayOptions.save();
+    }
     await interaction.reply({
-      content: "Successfully set new information!",
+      content: "Successfully set new information!"
     });
     if (userOptions && emailOnly === false) {
       await interaction.followUp({
         content:
-          "To confirm that email-only mode will work, the bot will attempt to send a test screenshot.",
+          "To confirm that email-only mode will work, the bot will attempt to send a test screenshot."
       });
       try {
         const user: User = interaction.user;
@@ -163,7 +233,7 @@ module.exports = {
           interaction.user.id,
           {
             itemType: ItemType.user,
-            item: user,
+            item: user
           }
         );
       } catch (e) {
@@ -176,10 +246,11 @@ module.exports = {
           );
           userOptions.emailOnly = true;
           await userOptions.save();
-        } else {
+        }
+        else {
           throw e;
         }
       }
     }
-  },
+  }
 };
