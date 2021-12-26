48adf7992fa20a56da317b2b5e2914c3552f2755
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2021-12-25 22:49:09 -0500

    Add supplementor system

---
+++ src/utils/logError/supplementors/sequelizeValidationError.ts
@@ -0,0 +1,10 @@
+import { ValidationError } from "sequelize";
+
+export default function sequelizeValidationError(error: ValidationError, type: string, metadata ?: object): object {
+  return {
+    original: metadata || null,
+    supplemented: {
+      validationErrors: error.errors
+    }
+  }
+}
