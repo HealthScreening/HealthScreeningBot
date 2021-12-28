a4204f6bea9bfed5d00830dc9cfd75c296bdf66a
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2021-12-28 10:42:28 -0500

    Run

--- scripts/automaticallyAddGPLToCode.ts
+++ scripts/automaticallyAddGPLToCode.ts
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2021 PythonCoderAS
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 import pathsGenerator = require("walk-asyncgen")
 import { readFile, writeFile } from "fs/promises"
 import { resolve } from "path"
@@ -29,7 +45,9 @@ async function processFile(path: string){
 
 async function main(){
   const promises: Promise<void>[] = [];
-  for await (const path of pathsGenerator(resolve(__dirname, ".."))) {
+  for await (const path of pathsGenerator(resolve(__dirname, ".."), {
+    excludeDirs: /(node_modules|\.git)\/?.*/
+  })) {
     if (path.endsWith(".ts")){
       promises.push(processFile(path))
     }
