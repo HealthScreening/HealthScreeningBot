a7ff13aab64ad7fbbc8b79ef0fb0ce446c9d05da
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2021-12-28 13:39:26 -0500

    Add command log model

---
+++ src/orm/commandLog.ts
@@ -0,0 +1,71 @@
+/**
+ * Copyright (C) 2021 PythonCoderAS
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+import { DataTypes, Model, Optional } from "sequelize";
+
+import { sequelize } from ".";
+
+export interface CommandLogAttributes {
+  id: number;
+  userId: string;
+  commandName: string;
+}
+
+export type CommandLogCreationAttributes = Optional<
+  CommandLogAttributes,
+  "id"
+>;
+
+export class CommandLog
+  extends Model<CommandLogAttributes, CommandLogCreationAttributes>
+  implements CommandLogAttributes
+{
+  id!: number;
+  userId!: string;
+  commandName!: string;
+}
+
+CommandLog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    userId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    commandName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: "CommandLog",
+    timestamps: true,
+    updatedAt: false,
+    indexes: [
+      {
+        fields: ["userID"],
+      },
+      {
+        fields: ["commandName"],
+      }
+    ],
+  }
+);
---
+++ src/orm/migrations/models/commandlog.ts
@@ -0,0 +1,64 @@
+/**
+ * Copyright (C) 2021 PythonCoderAS
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+import { DataTypes as DT, Model, Sequelize } from "sequelize";
+import { CommandLog } from "../../commandLog";
+
+module.exports = (sequelize: Sequelize, DataTypes: typeof DT) => {
+  class CommandLog extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate() {
+      // define association here
+    }
+  }
+
+  CommandLog.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      userId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      commandName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "CommandLog",
+      timestamps: true,
+      updatedAt: false,
+      indexes: [
+        {
+          fields: ["userID"],
+        },
+        {
+          fields: ["commandName"],
+        }
+      ],
+    }
+  );
+  return CommandLog;
+};
