2ce1c6bad84a4466a42da51f33e565182994133c
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2021-12-28 12:51:26 -0500

    Add error log prune command

---
+++ src/commands/error/log/prune/index.ts
@@ -0,0 +1,192 @@
+/**
+ * Copyright (C) 2021 PythonCoderAS
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+import { SlashCommandSubcommandBuilder } from "@discordjs/builders";
+import {
+  AutocompleteInteraction,
+  Collection,
+  CommandInteraction,
+  MessageEmbed,
+} from "discord.js";
+import { DateTime } from "luxon";
+import { Op, col, fn, where } from "sequelize";
+
+import { Subcommand } from "../../../../client/command";
+import { ErrorLog } from "../../../../orm/errorLog";
+import afterAutocomplete from "../view/autocomplete/after";
+import afterTimeAutocomplete from "../view/autocomplete/afterTime";
+import beforeAutocomplete from "../view/autocomplete/before";
+import beforeTimeAutocomplete from "../view/autocomplete/beforeTime";
+import typeStartsWithAutocomplete from "../view/autocomplete/typeStartsWith";
+
+export default class ErrorLogViewCommand extends Subcommand {
+  public readonly autocompleteFields: Collection<
+    string,
+    (interaction: AutocompleteInteraction) => Promise<void>
+  > = new Collection(
+    Object.entries({
+      type_starts_with: typeStartsWithAutocomplete,
+      before: beforeAutocomplete,
+      after: afterAutocomplete,
+      after_time: afterTimeAutocomplete,
+      before_time: beforeTimeAutocomplete,
+    })
+  );
+  registerSubcommand(
+    subcommand: SlashCommandSubcommandBuilder
+  ): SlashCommandSubcommandBuilder {
+    return subcommand
+      .setName("prune")
+      .setDescription("Prune the error log.")
+      .addIntegerOption((option) =>
+        option
+          .setName("before")
+          .setDescription("Prune the errors before this error #")
+          .setRequired(false)
+          .setAutocomplete(true)
+      )
+      .addIntegerOption((option) =>
+        option
+          .setName("after")
+          .setDescription("Prune the errors after this error #")
+          .setRequired(false)
+          .setAutocomplete(true)
+      )
+      .addIntegerOption((option) =>
+        option
+          .setName("after_time")
+          .setDescription("Prune errors after the given UNIX timestamp")
+          .setRequired(false)
+          .setAutocomplete(true)
+      )
+      .addIntegerOption((option) =>
+        option
+          .setName("before_time")
+          .setDescription("Prune errors before the given UNIX timestamp")
+          .setRequired(false)
+          .setAutocomplete(true)
+      )
+      .addStringOption((option) =>
+        option
+          .setName("type_starts_with")
+          .setDescription(
+            "Prunes the errors with a type starting with the given string"
+          )
+          .setRequired(false)
+          .setAutocomplete(true)
+      )
+      .addIntegerOption((option) =>
+        option
+          .setName("limit")
+          .setDescription("Limit the number of errors pruned")
+          .setRequired(false)
+      )
+      .addBooleanOption((option) =>
+        option
+          .setName("ephemeral")
+          .setDescription(
+            "Whether or not the contents are hidden to everyone else"
+          )
+          .setRequired(false)
+      );
+  }
+  async execute(interaction: CommandInteraction) {
+    const whereQuery: { [k: string]: object } = {};
+    const before: number | null = interaction.options.getInteger("before");
+    const after: number | null = interaction.options.getInteger("after");
+    const beforeTime: number | null =
+      interaction.options.getInteger("before_time");
+    const afterTime: number | null =
+      interaction.options.getInteger("after_time");
+    const typeStartsWith: string | null =
+      interaction.options.getString("type_starts_with");
+    const limit: number | null = interaction.options.getInteger("limit");
+    if (before) {
+      if (!whereQuery.id) {
+        whereQuery.id = {};
+      }
+      whereQuery.id[Op.lt] = before;
+    }
+    if (after) {
+      if (!whereQuery.id) {
+        whereQuery.id = {};
+      }
+      whereQuery.id[Op.gt] = after;
+    }
+    if (beforeTime) {
+      if (!whereQuery.createdAt) {
+        whereQuery.createdAt = {};
+      }
+      whereQuery.createdAt[Op.lt] = new Date(beforeTime * 1000);
+    }
+    if (afterTime) {
+      if (!whereQuery.createdAt) {
+        whereQuery.createdAt = {};
+      }
+      whereQuery.createdAt[Op.gt] = new Date(afterTime * 1000);
+    }
+    if (typeStartsWith) {
+      whereQuery.type = where(fn("lower", col("type")), {
+        [Op.startsWith]: typeStartsWith.toLowerCase(),
+      });
+    }
+    const deleted = await ErrorLog.destroy({
+      where: whereQuery,
+      limit: limit || undefined,
+    });
+    const embed = new MessageEmbed();
+    embed.setTitle("Pruned Error Log");
+    embed.setDescription(`Items Deleted: **${deleted}**`);
+    embed.setColor(deleted > 0 ? "GREEN" : "RED");
+    let fieldData: string = ""
+    if (before) {
+      fieldData += `\nBefore: **#${before}**`;
+    } else {
+      fieldData += "\nBefore: **None**";
+    }
+    if (after) {
+      fieldData += `\nAfter: **#${after}**`;
+    } else {
+      fieldData += "\nAfter: **None**";
+    }
+    if (beforeTime) {
+      fieldData += `\nBefore Time: **${DateTime.fromMillis(
+        beforeTime * 1000
+      ).toLocaleString(DateTime.DATETIME_HUGE_WITH_SECONDS)}**`;
+    } else {
+      fieldData += "\nBefore Time: **None**";
+    }
+    if (afterTime) {
+      fieldData += `\nAfter Time: **${DateTime.fromMillis(
+        afterTime * 1000
+      ).toLocaleString(DateTime.DATETIME_HUGE_WITH_SECONDS)}**`;
+    } else {
+      fieldData += "\nAfter Time: **None**";
+    }
+    if (typeStartsWith) {
+      fieldData += `\nType Starts With: **\`${typeStartsWith}\`**`;
+    } else {
+      fieldData += "\nType Starts With: **None**";
+    }
+    embed.addField("Search Properties", fieldData);
+    const ephemeral =
+      interaction.options.getBoolean("ephemeral", false) ?? true;
+    await interaction.reply({
+      embeds: [embed],
+      ephemeral,
+    });
+  }
+}
