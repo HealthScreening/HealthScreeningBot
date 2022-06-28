427ac341a26c756bb7f45f5cf4c3fe98d58feb40
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-06-27 22:30:24 -0400

    Change for the summer

--- src/client/doAutoChangePresence.ts
+++ src/client/doAutoChangePresence.ts
@@ -1,25 +1,13 @@
 import { sample } from "lodash";
 
-import { AutoUser } from "../orm/autoUser";
 import HealthScreeningBotClient from "./extraClient";
 
 export default async function doAutoChangePresence(
   client: HealthScreeningBotClient
 ): Promise<void> {
-  const guildSize = client.guilds.cache.size;
-  const registeredPeople = await AutoUser.count();
 
   const presences: string[] = [
-    "Generating Health Screenings",
-    `In ${guildSize} servers`,
-    `Generating for ${registeredPeople} people!`,
-    "/guide name:walkthrough for walkthrough of commands!",
-    "Report a bug with /report_bug!",
-    "Use /set to set optional configuration info!",
-    "can i put ma balls on ya jaws",
-    "Run /stats for cool stats about the bot!",
-    "Check out your profile with /profile!",
-    "Have an idea? Suggest it with /suggest!",
+    "Paused for the summer! Use /set to unpause."
   ];
 
   // We only need to set the name.
