6468057c888b1ee148ef32ecf3c77bdeb9865d2e
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-06-27 22:52:32 -0400

    Fix lint issues

--- src/client/doAutoChangePresence.ts
+++ src/client/doAutoChangePresence.ts
@@ -5,10 +5,7 @@ import HealthScreeningBotClient from "./extraClient";
 export default async function doAutoChangePresence(
   client: HealthScreeningBotClient
 ): Promise<void> {
-
-  const presences: string[] = [
-    "Paused for the summer! Use /set to unpause."
-  ];
+  const presences: string[] = ["Paused for the summer! Use /set to unpause."];
 
   // We only need to set the name.
   await client.user!.setPresence({ activities: [{ name: sample(presences) }] });
