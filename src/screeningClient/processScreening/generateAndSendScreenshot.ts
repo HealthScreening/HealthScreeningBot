f1a114bb6eacb6819b0cd3182e627e9ef8863224
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-07-20 13:53:33 -0400

    Fix screenshot generation

--- src/screeningClient/processScreening/generateAndSendScreenshot.ts
+++ src/screeningClient/processScreening/generateAndSendScreenshot.ts
@@ -1,4 +1,4 @@
-import { ActionRowBuilder } from "discord.js";
+import { ActionRowBuilder, AttachmentBuilder, ButtonBuilder } from "discord.js";
 
 import { AutoUser } from "../../orm/autoUser";
 import getPresetButtonBuilder from "../../utils/buttonPresets";
@@ -30,18 +30,12 @@ export default async function generateAndSendScreenshot(params: ProcessParams) {
 
     const messageParams: MessageOptions = {
       content: "Here is the screenshot that you requested:",
-      files: [
-        {
-          attachment: screenshot,
-          name: "screening.jpg",
-          file: screenshot,
-        },
-      ],
+      files: [ new AttachmentBuilder(screenshot).setName("screening.jpg") ],
       ...params.multiMessageParams,
     };
     if (messageParams.itemType === ItemType.user) {
       messageParams.components = [
-        new ActionRowBuilder().addComponents(getPresetButtonBuilder("delete")),
+        new ActionRowBuilder<ButtonBuilder>().addComponents(getPresetButtonBuilder("delete")),
       ];
     }
 
